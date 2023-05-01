8652fdfd300bb9b4c16bc104aaa6ea5babc16508
Ali Glenesk <ali.glenesk@gmail.com>
2023-05-01 07:58:20 -0700

    add home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 const Nav = ({loggedIn}) => {
     return (
         <nav className="navMenu">
-            <img src="logo.png" alt="stroller" height="50px"/>
+            <a href="/"><img src="logo.png" alt="stroller" height="50px"/></a>
             <div id="links-container">
+                {loggedIn && <a href="/">Home</a>}
                 {loggedIn && <a href="/about">About</a>}
                 {loggedIn && <a href="/" onClick={() => window.localStorage.clear()}>Logout</a>}
             </div>
