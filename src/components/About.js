0bbd975ecba3fc876eb7376875aec593659802bc
Ali Glenesk <ali.glenesk@gmail.com>
2023-05-06 19:05:30 -0700

    miau

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,8 @@ const About = () => {
             {location.pathname === "/about" && (
                 <div>
                     <h1>FAQ</h1>
+                    <h2>How do I track my stroller stats?</h2>
+                    <p>Add #strollerstats to your activity description. #strollermiles will work too! You have to update your description at the same time that you make a title change so that Strava's webhook will send your changes.</p>
                     <h2>How can I add stroller miles from past activities?</h2>
                     <p>I haven't made a 'click to pull in all your past activities' mechanism yet. However, if you add #strollerstats to your activity description and also make a small title change at the same time, the app will pull it in. Unfortunately you do have to change the title at the same time due to how Strava webhooks work!</p>
                     <h2>How can I report a bug or submit a feature request?</h2>
