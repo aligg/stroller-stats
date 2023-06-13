2de69bee0a9cb15b9bd026a8f2db0fe034f283a4
Ali Glenesk <ali.glenesk@gmail.com>
2023-06-13 10:04:46 -0400

    maybe fix powered by image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <>
-                <img src="powered-by-strava.png" alt="Powered by Strava" style={{height:"35px"}}/>
+                <img src="/powered-by-strava.png" alt="Powered by Strava" style={{height:"35px"}}/>
                 <p id="footer-text">❤️Made during naptime by <a href="https://github.com/aligg" target="_blank" rel="noreferrer">aligg</a>
                 </p>
             </>
