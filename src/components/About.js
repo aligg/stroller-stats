753d7d1bd20b45943749aeddbf7f2687f33b6ecd
Ali Glenesk <ali.glenesk@gmail.com>
2023-06-22 14:59:50 -0700

    enable only me

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ const About = () => {
         <main>
             <h1>Track your stroller miles with Stroller Stats.</h1>
             <ul>
-                <li>Add #strollerstats to the description of any Strava walk or run and we'll track your stroller miles</li>
-                <li>When you grant write access, we'll also update your description to share month to date stroller mileage from your activity</li>
-                <li>You can revoke access anytime</li>
+                <li>Add #strollerstats to the description of any Strava walk or run and we'll track your stroller miles.</li>
+                <li>When you grant write access, we'll also update your description to share month to date stroller mileage from your activity. If you'd like stroller stats to be counted for "Only Me" activities, keep "View data about your private activities checked."</li>
+                <li>You can revoke access anytime.</li>
                 <li>Compete with friends and family to see who is getting that baby out the most!</li>
             </ul>
             {location.pathname === "/about" && (
--- src/routes/home.js
+++ src/routes/home.js
@@ -10,7 +10,7 @@ const Home = () => {
     const { REACT_APP_CLIENT_ID } = process.env;
     const redirectUrl = process.env.NODE_ENV === "development"? "http://localhost:3000/redirect" : "http://strollerstats.com/redirect"
     const handleLogin = () => {
-        window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,activity:read,activity:write`;
+        window.location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=read,activity:read,activity:write,activity:read_all`;
     };
     const user_id = localStorage.getItem("user_id");
     const [data, dataLoading] = useData(user_id)
