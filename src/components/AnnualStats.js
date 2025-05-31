4d6cd118a1b16ddc8b3367939a3ea1afc08e1775
Ali Glenesk <ali.glenesk@gmail.com>
2025-05-31 15:12:11 -0700

    more labels

--- src/components/AnnualStats.js
+++ src/components/AnnualStats.js
@@ -26,7 +26,8 @@ const AnnualStats = ({userId}) => {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false)
     const currYear = startYear + index
-    console.log(data)
+    const distanceLabel = data["distance_unit"]
+
     useEffect(() => {
         
         const getData = async (userId) => {
@@ -58,20 +59,20 @@ const AnnualStats = ({userId}) => {
                                 <th>{getAffirmation(data.first_name)}</th>
                             </tr>  
                             <tr>
-                                <td>Total stroller run miles</td>
+                                <td>{`Total stroller run ${distanceLabel}s`}</td>
                                 <td>{data["total_run_distance"].toFixed(2)}</td>
                             </tr>
                             <tr>
-                                <td>Total stroller walk miles</td>
+                                <td>{`Total stroller walk ${distanceLabel}s`}</td>
                                 <td>{data["total_walk_distance"].toFixed(2)}</td>
                             </tr>
                             <tr>
                                 <td>Average run pace with stroller</td>
-                                <td>{data["average_run_speed"] === null ? "N/A" : `${data["average_run_speed"]} min/${data["distance_unit"]}`}</td>
+                                <td>{data["average_run_speed"] === null ? "N/A" : `${data["average_run_speed"]} min/${distanceLabel}`}</td>
                             </tr>
                             <tr>
                                 <td>Average walk pace with stroller</td>
-                                <td>{data["average_walk_speed"] === null ? "N/A" : `${data["average_walk_speed"]} min/${data["distance_unit"]}`}</td>
+                                <td>{data["average_walk_speed"] === null ? "N/A" : `${data["average_walk_speed"]} min/${distanceLabel}`}</td>
                             </tr>
                         </tbody>
                 </table>
@@ -93,7 +94,7 @@ const AnnualStats = ({userId}) => {
 
     return (
     <>
-        <h1>Annual stroller miles</h1>
+        <h1>{`Annual stroller ${distanceLabel}s`}</h1>
             <Tabs defaultIndex={index} onSelect={(index) => {setIndex(index)}}>
                 <TabList>
                     {generateTabsForEachYear()}
