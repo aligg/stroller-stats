01e3da27b89de813f19bc52c6257f791cdc0805f
Ali Glenesk <ali.glenesk@gmail.com>
2026-01-16 10:21:39 -0800

    maybe fix build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './index.css';
 import App from "./App";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import firebase from "./utils/firebase"
 
 const container = document.getElementById('root');
 const root = createRoot(container);
